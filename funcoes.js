6b381cea1d3987e3ca87da3b4ecf7815bedc2f7f
CaioZanardo <caio_zanardo@hotmail.com>
2023-06-05 23:40:07 -0300

    ADD Exerc

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function exercicioQuatro(){
+	
+	cont = 0
+	numero = 0
+
+	while(numero != 2){ 
+		numero = prompt("Digite um valor: ")
+		cont++;
+	}
+	alert("Agora o programa será finalizado"); 
+}
